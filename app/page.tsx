e23d6018d361fab4ba705d408da9284bcb458f30
NshIS <82755033+NazarShuk@users.noreply.github.com>
2024-12-10 19:59:58 +0000

    main page

--- app/page.tsx
+++ app/page.tsx
@@ -3,15 +3,19 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div className="w-full h-full flex flex-col justify-center items-center">
-      <h1 className="text-4xl">Study for your test easier and simpler</h1>
-      <div className="w-full h-fit p-1 flex justify-between items-center gap-2.5 text-xl text-blue-500">
-        <Link className="underline" href={"/new"}>
-          Create your own
-        </Link>
-        <Link className="underline" href={"/new/clone"}>
-          Clone from Quizlet
-        </Link>
+      <div className="w-full h-screen translate-y-[-3em] flex justify-center items-center flex-col">
+        <h1 className="text-4xl bg-gradient-to-r from-black to-blue-500 bg-clip-text text-transparent font-extrabold">Study for your test easier and simpler</h1>
+        
+        <div className="w-full h-fit p-1 flex justify-center items-center gap-2.5 text-xl text-blue-500">
+          <Link className="text-black bg-gray-100 p-2.5 rounded" href={"/new"}>
+            Create your own
+          </Link>
+          <Link className="text-black bg-gray-100 p-2.5 rounded" href={"/new/clone"}>
+            Clone from Quizlet
+          </Link>
+        </div>
       </div>
+
     </div>
   );
 }
